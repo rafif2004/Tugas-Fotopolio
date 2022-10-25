fef4e44c1b879884af135616d4118ab466e1fcce
rafif2004 <Rafif.aja14@gmail.com>
2022-10-25 11:35:07 +0700

    p

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,7 +20,7 @@ class LoginController extends Controller
         if (Auth::attempt($data)) {
             $request->session()->regenerate();
  
-            return redirect()->intended('master.dashboard');
+            return redirect()->intended('/dashboard');
         }
  
         return back()->withErrors([
